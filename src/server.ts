4e7593cc7072aec2c46f943b513362136f39a1d7
alessandro.uleon <alessandro.uleon@grupoicts.com.br>
2023-01-18 18:55:34 -0400

    docs: implementation swagger

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import 'dotenv/config';
-
 import express from 'express';
-
 import { userRouter } from './routes/users.routes';
 import { specialityRouter } from './routes/speciality.routes';
+import swaggerUI  from 'swagger-ui-express';
+
+import swaggerDocument from '../swagger.json'
 
 const app = express();
 
@@ -11,6 +12,8 @@ app.use(express.json());
 
 const port = 3333;
 
+
+app.use('/docs-api', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 app.use('/user', userRouter);
 app.use('/speciality', specialityRouter);
 
